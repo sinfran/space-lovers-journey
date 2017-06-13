8bce8d0b93f08c0d29d6efbcabea421cb6b500df
Chris Yoon <Chris Yoon>
2017-06-12 20:34:52 -0700

    Kite flies without collision issues

--- Assets/Scripts/KiteDevice.cs
+++ Assets/Scripts/KiteDevice.cs
@@ -2,49 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KiteDevice : MonoBehaviour {
+public class KiteDevice : MonoBehaviour
+{
     public float kiteSpeed = 10.0f;
-    public enum State {DORMANT, GET_ON_KITE, FLY };
+    public enum State { DORMANT, GET_ON_KITE, FLY };
     public State s;
     private GameObject player;
-    public Vector3 kitePos;
-    public float mountSpeed = 3.0f;
-
-	void Start () {
+    private GameObject kiteContainer;
+    private bool bound = false;
+    void Start()
+    {
         s = State.DORMANT;
-        kitePos = transform.parent.gameObject.transform.localPosition;
-        print(kitePos);
-	}
+        kiteContainer = transform.parent.gameObject;
+    }
     //TODO: Handle offset between person and kite
     void Update()
     {
-        
+
         switch (s)
         {
             case (State.DORMANT):
                 break;
             case (State.GET_ON_KITE):
-                //TODO: This logic is faulty we think... but we can skip this because fly positions kite underneath player
-                //Vector3 playerPos = player.transform.position;
-                //playerPos = Vector3.MoveTowards(playerPos, kitePos, mountSpeed*Time.deltaTime);
-                //player.transform.position = playerPos;
                 s = State.FLY;
                 break;
             case (State.FLY):
+                //Bind kite to player
                 Transform camera = player.transform.GetChild(0).transform;
-                Vector3 direction = player.transform.GetChild(0).transform.forward;
-                GameObject kiteContainer = transform.parent.gameObject;
-                direction *= Time.deltaTime * 6.0f;
-                Rigidbody kiteBody = kiteContainer.GetComponent<Rigidbody>();
-                kiteBody.MovePosition(transform.position + direction);
+                CharacterController playercontroller = player.GetComponent<CharacterController>();
+                if (!bound)
+                {
+                    player.transform.position += new Vector3(0, 2.0f, 0);
+                    kiteContainer.transform.parent = player.transform;
+                    kiteContainer.transform.localPosition = new Vector3(0, -1, 0);
+                    //Position kite under
+                    bound = true;
+                }
 
-                //Moving person on top of kite
-                Vector3 playerTargetPos = kiteContainer.transform.position;
-                playerTargetPos.y += 2f;
-                player.transform.position = playerTargetPos;
+                //Move person   
+                Vector3 cameraForward = camera.forward;
+                playercontroller.Move(cameraForward * Time.deltaTime * kiteSpeed);
 
-                ////rotating kite
-                kiteContainer.transform.rotation = Quaternion.Lerp(kiteContainer.transform.rotation, camera.rotation, 0.5f*Time.deltaTime);
+                //rotating kite
+                kiteContainer.transform.rotation = Quaternion.Lerp(kiteContainer.transform.rotation, camera.rotation, 10f * Time.deltaTime);
+
+
                 break;
             default:
                 print("Invalid state");
@@ -53,17 +55,16 @@ public class KiteDevice : MonoBehaviour {
     }
 
     // Operate is called by Device Operator
-    public void Operate (DeviceOperator devo) {
+    public void Operate(DeviceOperator devo)
+    {
         print("Kite Operate called.");
 
         //Logic to move the person on top of the kite
         Person p = devo.GetComponent<Person>();
         p.canMove = false;
-
         player = p.gameObject;
 
-        s = State.GET_ON_KITE;      
-	}
+        s = State.GET_ON_KITE;
+    }
 
 }
-
