839b3ddba8b3658f51a989927697cd84c01c8754
sinfran <frances.sin31@gmail.com>
2017-06-15 19:19:52 -0700

    Worked on planet, added colliders to astroids, tweaked colour of sprites

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instructions : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (KiteDevice.s != KiteDevice.State.INACTIVE) {
+			if (GetComponent<CanvasGroup> ().alpha > 0) {
+				GetComponent<CanvasGroup> ().alpha -= 0.006f;
+			}
+		}
+	}
+}
