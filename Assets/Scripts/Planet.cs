247b3413965210afa3210b43712c9816db8e5ce0
Chris Yoon <Chris Yoon>
2017-06-21 21:56:22 -0700

    Applied forced to character. Need to rotate to normal

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour {
-
+    private bool x = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,6 +14,20 @@ public class Planet : MonoBehaviour {
 		
 	}
 
+    void OnCollisionEnter(Collision collision)
+    {   
+        if (!x)
+        {
+            print("hit planet!");
+            Vector3 collisionNormal = collision.contacts[0].normal;
+            collision.transform.rotation = Quaternion.Euler(collisionNormal);
+            collision.rigidbody.freezeRotation = true;
+            print(collisionNormal);
+            x = true;
+        }
+        
+
+    }
 
     void OnTriggerEnter(Collider collider)
     {
@@ -21,21 +35,29 @@ public class Planet : MonoBehaviour {
 
         if (collider.gameObject.name.Equals("KiteContainer"))
         {
-            Transform player = collider.transform.parent;
-
-            Vector3 playerPos = player.position;
-
-            Vector3 velocity = playerPos - transform.position;
-
-            CharacterController controller = player.gameObject.GetComponent<CharacterController>();
-
-          
-            
-
-            //collider.gameObject.transform.parent
+            Transform playerTransform = collider.transform.parent;
             Destroy(collider.gameObject);
+
+            Vector3 playerPos = playerTransform.position;
+            Vector3 velocity = transform.position - playerPos;
+
+            CharacterController controller = playerTransform.gameObject.GetComponent<CharacterController>();
+
+
+            GameObject player = playerTransform.gameObject;
+            Rigidbody rb = player.AddComponent<Rigidbody>();
+            rb.angularDrag = 0;
+            rb.useGravity = false;
+
+            rb.AddForce(velocity * 0.05f, ForceMode.VelocityChange);
+      
+
+
+            
+            //collider.gameObject.transform.parent
+           
         }
     }
-
+  
 
 }
